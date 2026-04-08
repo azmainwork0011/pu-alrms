9b123bbd56f530ba177f2d110bbefdbdb10f6b9f
Z User <z@container>
2026-04-08 17:39:21 +0000

    9032c3d7-2599-429a-8517-75f8c0d5e8e0

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,10 +4,17 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query'],
+function createPrismaClient() {
+  return new PrismaClient({
+    log: ['error', 'warn'],
   })
+}
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+export const db = createPrismaClient()
+
+// In development, cache on globalThis but always validate the client has the models we need
+if (process.env.NODE_ENV !== 'production') {
+  if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = db
+  }
+}
